1221a0e14f04137550c5ec105a5ea163f9cd2c8a
alex kadai <alexandrkadai@gmail.com>
2022-10-20 17:08:30 +0300

    MOre tests

--- script.async.test.js
+++ script.async.test.js
@@ -16,4 +16,25 @@ describe('googleSearch', () => {
       expect(data.results.length).toBeGreaterThan(5);
     });
   });
+
+  it('gets people and return counts and results', () => {
+    const mockFetch = jest.fn().mockReturnValue(
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            count: 87,
+            results: [0, 1, 2, 3, 4, 5],
+          }),
+      }),
+    );
+    expect.assertions(4);
+    return swapi.getPeopleAsync(mockFetch).then((data) => {
+      expect(mockFetch.mock.calls.length).toBe(1);
+      expect(mockFetch).toBeCalledWith('https://swapi.py4e.com/api/people');
+      expect(data.count).toEqual(87);
+      expect(data.results.length).toBeGreaterThan(5);
+    });
+  });
+
+  //Describe End
 });
